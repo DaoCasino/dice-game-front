3e36ab7de7312dac8737cfa1b1216b95f00b04a3
Gar|k <garik.djan@gmail.com>
2020-07-31 17:16:04 +0300

    feat: betOnWin increase maxBet

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -354,8 +354,8 @@ export default class MainScreen extends BaseScreen {
 
           if (betOnWin > 0 && profit > 0) {
             if (betOnWinAction === 'increase') {
-              this.gameModel.set('bet', Math.min(this.gameModel.get('balance'), parseFloat((bet + bet * betOnWin / 100).toFixed(4))))
-
+              const newBet = Math.min(balance, parseFloat((bet + bet * betOnWin / 100)))
+              this.gameModel.set('bet', Math.min(betMax, newBet))
             } else if (betOnWinAction === 'decrease') {
               this.gameModel.set('bet', Math.max(this.gameModel.get('betMin'), parseFloat((bet - bet * betOnWin / 100).toFixed(4))))
             }
